12f21bde47773eba1a718abd339fe24036fdfabc
xiaowu-github <qazcz@126.com>
2015-04-24 12:36:48 -0400

    Create diagram.js

---
+++ diagram.js
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>  
+	<meta charset="utf-8">  
+	<title>Score Visualization for 2014 FIFA World Cup Knockout Stage </title>  
+	<link rel="stylesheet" type="text/css" href="style.css" title="Style 1"/>
+</head> 
+
+
+
+<body>  
+	<div class="header">
+		<h5>PROG8150-15W-Sec1-User Interface Design Principles</h5>
+		<h1>Final Project: <br>Score Visualization for 2014 FIFA World Cup Knockout Stage</h1>
+		<h3>Made by:  Chaozhi Zhang; Mehmood Chaudhry; Ramanpreet Kaur; Yogesh Bhoj</h3>
+		
+	</div>
+	
+	<div class="main">
+		<div id="diagram">
+			<script src="http://d3js.org/d3.v3.min.js"></script>
+			<script src="diagram.js"></script>
+		</div>
+	</div>
+	<div class="referances">
+		<h3>Referances:</h3>
+		<p>1. <a href="http://en.wikipedia.org/wiki/2014_FIFA_World_Cup_knockout_stage" >2014 FIFA World Cup knockout stage - Wikipedia</a></p>
+		<p>2. <a href="http://bl.ocks.org/mbostock/4062006" >Chord Diagram - Ocks</a></p>
+	</div>
+		
+	<div class="footer" onmouseover="this.style.color='red'" onmouseout="this.style.color='gray'" >
+		This website is property of Chaozhi Zhang; Mehmood Chaudhry; Ramanpreet Kaur; Yogesh Bhoj. <br>All rights reserved. © 2015
+	</div>
+		
+</body>  
+
+</html>  
